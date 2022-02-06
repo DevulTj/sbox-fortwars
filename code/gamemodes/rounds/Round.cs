a30ed924bec07574e2f0a380b6afc11e09a58433
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-06 11:08:30 +1100

    Initial Commit, Create Rounds and Gamemode

---
+++ code/gamemodes/rounds/Round.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Sandbox;
+
+namespace Fortwars.Gamemodes;
+
+public abstract partial class Round : BaseNetworkable
+{
+	[Net] public bool IsFinished { get; private set; }
+	[Net] public float StartTime { get; private set; }
+
+	// Helpers
+	public TimeSpan ElapsedTime => !IsFinished ? TimeSpan.FromSeconds( Time.Now - StartTime ) : TimeSpan.Zero;
+
+	public virtual void Start()
+	{
+		Host.AssertServer();
+
+		Log.Info( $"Round started {GetType()}" );
+
+		_ = ThinkTimer();
+
+		StartTime = Time.Now;
+		OnStart();
+	}
+
+	public virtual void Finish()
+	{
+		Host.AssertServer();
+
+		if ( IsFinished )
+		{
+			return;
+		}
+
+		Log.Info( $"Round ended {GetType()}" );
+
+		IsFinished = true;
+		OnFinish();
+	}
+
+	private async Task ThinkTimer()
+	{
+		while ( !IsFinished )
+		{
+			OnThink();
+			await GameTask.DelaySeconds( 1 );
+		}
+	}
+
+	protected virtual void OnStart() { }
+	protected virtual void OnFinish() { }
+	protected virtual void OnThink() { }
+}
---
+++ code/gamemodes/rounds/TimedRound.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Sandbox;
+
+namespace Fortwars.Gamemodes;
+
+public abstract partial class TimedRound : Round
+{
+	public virtual TimeSpan RoundDuration => TimeSpan.FromSeconds(60);
+	public TimeSpan TimeLeft => !IsFinished ? RoundDuration - ElapsedTime : TimeSpan.Zero;
+
+	[Net] private float EndTime { get; set; }
+
+	public override void Start() {
+		if(RoundDuration.TotalSeconds > 0) {
+			EndTime = Time.Now + (float)RoundDuration.TotalSeconds;
+		}
+
+		base.Start();
+	}
+
+	public override void Finish() {
+		if(Host.IsServer) {
+			EndTime = 0f;
+		}
+
+		base.Finish();
+	}
+
+	protected override void OnThink() {
+		if(!(EndTime > 0) || !(Time.Now >= EndTime)) {
+			return;
+		}
+
+		EndTime = 0f;
+		OnTimeUp();
+	}
+
+	protected virtual void OnTimeUp() {
+		Finish();
+	}
+}
